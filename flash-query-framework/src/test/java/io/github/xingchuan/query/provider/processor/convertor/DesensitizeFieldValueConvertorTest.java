cd2577c0977ad9c9c64c8f35f86f17b332c008a3
xingchuan.qxc <qianxingchuan@aliyun.com>
2022-07-03 18:16:49 +0800

    add test case .

---
+++ flash-query-framework/src/test/java/io/github/xingchuan/query/provider/processor/convertor/DesensitizeFieldValueConvertorTest.java
@@ -0,0 +1,35 @@
+package io.github.xingchuan.query.provider.processor.convertor;
+
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+/**
+ * @author xingchuan.qxc
+ * @date 2022/7/3
+ */
+public class DesensitizeFieldValueConvertorTest {
+
+    private Logger logger = LoggerFactory.getLogger(DesensitizeFieldValueConvertorTest.class);
+
+    @Test
+    public void test_脱敏测试() {
+        DesensitizeFieldValueConvertor convertor = new DesensitizeFieldValueConvertor();
+        String ret = convertor.processConvert("sample-value");
+        logger.info("{} ", ret);
+        assertThat(ret).isEqualTo("************");
+
+        JSONObject param = JSONUtil.createObj();
+        param.set("desensitizeType", "FIXED_PHONE");
+        String ret2 = convertor.processConvert("13000000019", param);
+        logger.info("{}", ret2);
+        assertThat(ret2).isEqualTo("1300*****19");
+
+
+    }
+}
